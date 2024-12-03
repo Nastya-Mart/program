1818420166fc5fca073c0b42dbfe4f4bb130a997
Nastya-Mart <nastya.mart749@gmail.com>
2024-12-03 20:11:21 +0300

    Add files via upload

---
+++ Введение в функциональное программирование.py
@@ -0,0 +1,16 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for i in functions:
+        results[i.__name__] = i(int_list)
+    return results
+int_list = [6, 20, 15, 9]
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
+
+
+
+
+
+
